5dad1f98fb6db5dc399c917f86d8bcffdf1233c7
angelgurrola <angel_gurrolac@outlook.com>
2017-04-02 23:56:29 -0500

    Languages and splash screen

--- js/index.js
+++ js/index.js
@@ -1,8 +1,15 @@
-var $item = $('.carousel .item'); 
+var $item = $('.carousel .item');
 var $wHeight = $(window).height();
 $item.eq(0).addClass('active');
 $item.height($wHeight); 
 $item.addClass('full-screen');
+
+  if (screen.width > 500) {
+$("#prueba").attr("src","img/enduring-company.jpg");
+}
+if (screen.width < 500) {
+$("#prueba").attr("src","img/4_Fondo_Security.jpg");
+}
 
 $('.carousel img').each(function() {
   var $src = $(this).attr('src');
@@ -26,16 +33,33 @@ $('.carousel').carousel({
 
 
 $(function(){
-  $("#logo2").css("display","none");
-  $(window).scroll(function(){
-    var aTop = 720;
-    if($(this).scrollTop()>=aTop){
+  if (screen.width < 500) {
+    $("#logo").detach();
+  }
+  if (screen.width > 500) {
+    $("#logo2").css("display","none");
+    $(window).scroll(function(){
+      var aTop = 720;
+      if($(this).scrollTop()>=aTop){
         $("#logo").css("display","none");
         $("#logo2").css("display","block");
-    }
-    if($(this).scrollTop()<=aTop){
+      }
+      if($(this).scrollTop()<=aTop){
         $("#logo").css("display","block");
         $("#logo2").css("display","none");
-    }
-  });
+      }
+    });
+  };
+
 });
+
+  $(document).ready(function(){
+      $('#english').on('click',function(){
+        window.open("index.html","_self");
+      });
+      $('#espanol').on('click',function(){
+        window.open("es.html","_self");
+      });
+
+
+  });
