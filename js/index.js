e092d7fe36ba41418d7567124d791e56a4b4c5e7
Zayra Chávez <zayrachavez@hotmail.com>
2017-04-06 22:43:01 -0500

    cambios

--- js/index.js
+++ js/index.js
@@ -45,7 +45,9 @@ $(".elements").html("<li data-target='#mycarousel' data-slide-to='0' class='acti
                 "<span class='glyphicon glyphicon-pause'></span>"+
                 "</button>");
 }
-
+if (screen.width <768) {
+  window.open ('us.html')
+}
 
 $('.carousel img').each(function() {
   var $src = $(this).attr('src');
