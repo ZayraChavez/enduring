d702ce909e7ac0ae05525f64d5644057bba42b5b
Zayra Chávez <zayrachavez@hotmail.com>
2017-04-14 14:20:25 -0500

    Final Version 1.0

--- js/index.js
+++ js/index.js
@@ -2,11 +2,36 @@ var $item = $('.carousel .item');
 var $wHeight = $(window).height();
 $item.eq(0).addClass('active');
 $item.height($wHeight);
-if (screen.width > 600) {
+if (screen.width > 800) {
   $item.addClass('img-responsive');
-$item.css("height","638");
+  $item.css("height","638");
 } 
-if (screen.width < 600) {
+if (screen.width == 1600 || screen.width == 1920) {
+  $item.addClass('full-screen');
+}
+if (screen.width == 1600) {
+   $item.css("height","738");
+  $(".background-network").css("height","1360");
+  $(".footer-above").css("height","411");
+  $(".footer-below").css("height","109");
+  $(".background-audio").css("height","1467");
+  $(".contact-section").css("height","900");
+    
+}
+if (screen.width == 1920) {
+    $item.css("height","838");
+  $(".background-network").css("height","1590");
+  $(".footer-above").css("height","490");
+  $(".footer-below").css("height","129");
+  $(".background-audio").css("height","1690");
+  $(".background-security").css("height","1690");
+  $(".contact-section").css("height","1070");
+
+
+}
+
+
+if (screen.width < 800) {
     $item.addClass('full-screen');
 }
 
@@ -22,7 +47,7 @@ if (screen.width < 600) {
                 }; 
         
 
-if (screen.width > 600) {
+if (screen.width > 800) {
 $("#img1").attr("src","img/slider-1.jpg");
 $("#img2").attr("src","img/slider-2.jpg");
 $("#img3").attr("src","img/slider-3.jpg");
@@ -35,6 +60,8 @@ $("#mobile-img1").detach();
 $("#mobile-img2").detach();
 $(".col-xs-8").removeClass("col-xs-offset-2");
 $(".col-xs-8").removeClass("col-xs-8");
+$(".tablet").css("display","none");
+$(".web").css("display","block");
 $(".elements").html("<li data-target='#mycarousel' data-slide-to='0' class='active'></li>"+
                 "<li data-target='#mycarousel' data-slide-to='1'></li>"+
                 "<li data-target='#mycarousel' data-slide-to='2'></li>"+
@@ -46,16 +73,45 @@ $(".elements").html("<li data-target='#mycarousel' data-slide-to='0' class='acti
                 "<button id='pauseButton2' type='button' class='pauseandplay btn btn-default btn-xs'>"+
                 "<span class='glyphicon glyphicon-pause'></span>"+
                 "</button>");
-
 }
 
-if (screen.width > 500 && screen.width < 650) {
+if (screen.width > 500 && screen.width < 800) {
     $("#img1").attr("src","img/slider-celh-1.jpg");
 $("#img2").attr("src","img/slider-celh-2.jpg");
 $("#img3").attr("src","img/slider-celh-3.jpg");
 $("#img4").attr("src","img/slider-celh-4.jpg");
 $("#img5").attr("src","img/slider-celh-5.jpg");
 $("#img6").attr("src","img/slider-celh-6.jpg");
+$("#bose").removeClass("bose");
+$("#alarm").removeClass("bose");
+$("#dsc").removeClass("bose");
+$("#bose2").removeClass("bose");
+$("#netgear").removeClass("netgear");
+$("#RemoveText").removeClass("text-center");
+$("#RemoveText2").removeClass("text-center");
+$("#RemoveText3").removeClass("text-center");
+$(".bolita-rosa2").css("display","inline-block");
+$(".pasta-text-cellphone").css("display","inline-block");
+$(".number-cellphone").css("display","block");
+$(".number-web").css("display","none");
+$(".pasta-text").css("display","none");
+$(".tamanoubitiqui").removeClass("tamanoubitiqui");
+$(".tamanoluxul").removeClass("tamanoluxul");
+$(".tamanonetgear").removeClass("tamanonetgear");
+$(".tamanocisco").removeClass("tamanocisco");
+$(".tamano2gig").removeClass("tamano2gig");
+$(".tamanoalarm").removeClass("tamanoalarm");
+$(".tamanolts").removeClass("tamanolts");
+$(".tamanohoneywell").removeClass("tamanohoneywell");
+$(".tamanodsc").removeClass("tamanodsc");
+$(".tamanohikvision").removeClass("tamanohikvision");
+$(".tamanopioneer").removeClass("tamanopioneer");
+$(".tamanoonkyo").removeClass("tamanoonkyo");
+$(".tamanoatlas").removeClass("tamanoatlas");
+$(".tamanobose").removeClass("tamanobose");
+$(".tamanoelura").removeClass("tamanoelura");
+$(".tablet").css("display","block");
+$(".web").css("display","none");
   }
 
 if (screen.width < 500) {
@@ -93,6 +149,8 @@ $(".tamanoonkyo").removeClass("tamanoonkyo");
 $(".tamanoatlas").removeClass("tamanoatlas");
 $(".tamanobose").removeClass("tamanobose");
 $(".tamanoelura").removeClass("tamanoelura");
+$(".tablet").css("display","none");
+$(".web").css("display","block")
 $(".elements").html("<li data-target='#mycarousel' data-slide-to='0' class='active'></li>"+
                 "<li data-target='#mycarousel' data-slide-to='1'></li>"+
                 "<li data-target='#mycarousel' data-slide-to='2'></li>"+
@@ -107,7 +165,7 @@ $(".elements").html("<li data-target='#mycarousel' data-slide-to='0' class='acti
                 "<span class='glyphicon glyphicon-pause'></span>"+
                 "</button>");
 }
-if (screen.width <768) {
+if (screen.width < 768) {
   window.open ('us.html')
 }
 
@@ -133,10 +191,10 @@ $('.carousel').carousel({
 
 
 $(function(){
-  if (screen.width < 600) {
+  if (screen.width < 800) {
     $("#logo").detach();
   }
-  if (screen.width > 600) {
+  if (screen.width > 800) {
     $("#logo2").css("display","none");
     $(window).scroll(function(){
       var aTop = 720;
--- js/indexEsp.js
+++ js/indexEsp.js
@@ -2,11 +2,36 @@ var $item = $('.carousel .item');
 var $wHeight = $(window).height();
 $item.eq(0).addClass('active');
 $item.height($wHeight); 
-if (screen.width > 700) {
+if (screen.width > 800) {
   $item.addClass('img-responsive');
 $item.css("height","638");
 } 
-if (screen.width < 700) {
+if (screen.width == 1600 || screen.width == 1920) {
+  $item.addClass('full-screen');
+}
+if (screen.width == 1600) {
+   $item.css("height","738");
+  $(".background-network").css("height","1360");
+  $(".footer-above").css("height","411");
+  $(".footer-below").css("height","109");
+  $(".background-audio").css("height","1467");
+  $(".contact-section").css("height","900");
+    
+}
+if (screen.width == 1920) {
+   $item.css("height","838");
+  $(".background-network").css("height","1590");
+  $(".footer-above").css("height","490");
+  $(".footer-below").css("height","129");
+  $(".background-audio").css("height","1690");
+  $(".background-security").css("height","1690");
+  $(".contact-section").css("height","1070");
+
+
+}
+
+
+if (screen.width < 800) {
     $item.addClass('full-screen');
 }
 
@@ -21,7 +46,7 @@ if (screen.width < 700) {
                 }; 
 
 
-if (screen.width > 700) {
+if (screen.width > 800) {
 $("#img1").attr("src","img/slideresp-1.jpg");
 $("#img2").attr("src","img/slideresp-2.jpg");
 $("#img3").attr("src","img/slideresp-3.jpg");
@@ -34,6 +59,8 @@ $("#mobile-img1").detach();
 $("#mobile-img2").detach();
 $(".col-xs-8").removeClass("col-xs-offset-2");
 $(".col-xs-8").removeClass("col-xs-8");
+$(".tablet").css("display","none");
+$(".web").css("display","block");
 $(".elements").html("<li data-target='#mycarousel' data-slide-to='0' class='active'></li>"+
                 "<li data-target='#mycarousel' data-slide-to='1'></li>"+
                 "<li data-target='#mycarousel' data-slide-to='2'></li>"+
@@ -45,16 +72,50 @@ $(".elements").html("<li data-target='#mycarousel' data-slide-to='0' class='acti
                 "<button id='pauseButton2' type='button' class='pauseandplay btn btn-default btn-xs'>"+
                 "<span class='glyphicon glyphicon-pause'></span>"+
                 "</button>");
-
 }
 
-if (screen.width > 500 && screen.width < 700) {
+
+
+if (screen.width > 500 && screen.width < 800) {
       $("#img1").attr("src","img/slider-us-celh-1.jpg");
       $("#img2").attr("src","img/slider-us-celh-2.jpg");
       $("#img3").attr("src","img/slider-us-celh-3.jpg");
       $("#img4").attr("src","img/slider-us-celh-4.jpg");
       $("#img5").attr("src","img/slider-us-celh-5.jpg");
       $("#img6").attr("src","img/slider-us-celh-6.jpg");
+
+      // nuevo
+      $("#bose").removeClass("bose");
+$("#alarm").removeClass("bose");
+$("#dsc").removeClass("bose");
+$("#bose2").removeClass("bose");
+$("#netgear").removeClass("netgear");
+$("#RemoveText").removeClass("text-center");
+$("#RemoveText2").removeClass("text-center");
+$("#RemoveText3").removeClass("text-center");
+$(".bolita-rosa2").css("display","inline-block");
+$(".pasta-text-cellphone").css("display","inline-block");
+$(".number-cellphone").css("display","block");
+$(".number-web").css("display","none");
+$(".pasta-text").css("display","none");
+$(".tamanoubitiqui").removeClass("tamanoubitiqui");
+$(".tamanoluxul").removeClass("tamanoluxul");
+$(".tamanonetgear").removeClass("tamanonetgear");
+$(".tamanocisco").removeClass("tamanocisco");
+$(".tamano2gig").removeClass("tamano2gig");
+$(".tamanoalarm").removeClass("tamanoalarm");
+$(".tamanolts").removeClass("tamanolts");
+$(".tamanohoneywell").removeClass("tamanohoneywell");
+$(".tamanodsc").removeClass("tamanodsc");
+$(".tamanohikvision").removeClass("tamanohikvision");
+$(".tamanopioneer").removeClass("tamanopioneer");
+$(".tamanoonkyo").removeClass("tamanoonkyo");
+$(".tamanoatlas").removeClass("tamanoatlas");
+$(".tamanobose").removeClass("tamanobose");
+$(".tamanoelura").removeClass("tamanoelura");
+$(".tablet").css("display","block");
+$(".web").css("display","none");
+      // nuevo
   }
 
 if (screen.width < 500) {
@@ -92,6 +153,8 @@ $(".tamanoonkyo").removeClass("tamanoonkyo");
 $(".tamanoatlas").removeClass("tamanoatlas");
 $(".tamanobose").removeClass("tamanobose");
 $(".tamanoelura").removeClass("tamanoelura");
+$(".tablet").css("display","none");
+$(".web").css("display","block");
 $(".elements").html("<li data-target='#mycarousel' data-slide-to='0' class='active'></li>"+
                 "<li data-target='#mycarousel' data-slide-to='1'></li>"+
                 "<li data-target='#mycarousel' data-slide-to='2'></li>"+
@@ -132,10 +195,10 @@ $('.carousel').carousel({
 
 
 $(function(){
-  if (screen.width < 600) {
+  if (screen.width < 800) {
     $("#logo").detach();
   }
-  if (screen.width > 500) {
+  if (screen.width > 800) {
     $("#logo2").css("display","none");
     $(window).scroll(function(){
       var aTop = 720;
